7c931530395396e0dab974f22b61f26223146019
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-05-21 23:11:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,6 +21,6 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include?()
+  cheese_types = ["cheddar", "gouda", "camembert"].include?("cheddar")
 end
