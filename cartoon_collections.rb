9a1657fe46768d7b9f0c3f1c110f9a69da4733d4
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-05-21 22:30:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,11 @@ end
 end
 
 def summon_captain_planet(array)
-  array.map {|hero| hero.capitalize
+  new_array = []
+  array.each do |hero|
+    new_array << hero.capitalize
+end
+new_array.push("!")
 end
 
 def long_planeteer_calls# code an argument here
