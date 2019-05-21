23eeaa4e701abc15fb12992ed1783ca103aae344
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-05-21 22:55:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,12 @@ end
 end
 
 def long_planeteer_calls(array)
-  
+  array.detect do |calls|
+    if calls.length > 4
+      true 
+    else 
+      false
+    end
 end
 
 def find_the_cheese# code an argument here
