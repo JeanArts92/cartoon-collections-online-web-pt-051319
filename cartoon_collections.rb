de656a009b0fdf8903049d6c488869260d03408e
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-05-21 22:20:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,7 @@ def roll_call_dwarves(array)
   array.each.with_index(1) do |dwarve, index|
     puts "#{index} #{dwarve}"
 end
+end
 
 def summon_captain_planet# code an argument here
   # Your code here
